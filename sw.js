1a13d87bf967d79d36fb9fee243dc0917060a0fb
Jordi Ferrés <jordi.ferres79@gmail.com>
2026-07-15 17:49:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'raw-tracker-v10';
+const CACHE_NAME = 'raw-tracker-v11';
 // Lista de archivos que queremos que se guarden en el móvil para usar offline
 const ASSETS = [
   './gym.trk-v5.html',
